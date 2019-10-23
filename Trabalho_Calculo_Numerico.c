e06a92c5eedcdff7e107bd4b3e077c188c6d01eb
Lucas Mota R M Cavalcante <36051351+lucasmrmc17@users.noreply.github.com>
2019-10-23 16:17:59 -0300

    Update Trabalho_Calculo_Numerico.c

--- Trabalho_Calculo_Numerico.c
+++ Trabalho_Calculo_Numerico.c
@@ -5,15 +5,20 @@
 double* dec2bin(double n);
 double* dec2oct(double n);
 double* dec2hex(double n);
+double **alocaMatriz(int L, int C);
+void leMatriz(double **M, int L, int C);
+void imprimeMatriz(double **M, int L, int C);
+void Jordan(double **M, int N);
 
 int main(){
-	char e;
-	double a;
+	int num_variaveis;
+	char escolha;
+	double a, **Matriz, *X;
 		
-	while(e != 'F'){
+	while(escolha != 'F'){
 		printf("Digite uma opcao:\nC - Conversao\nS - Sistema Linear\nE - Equacao Algebrica\nF - Finalizar\n");
-		scanf("%c", &e);
-		switch (e){
+		scanf("%c", &escolha);
+		switch (escolha){
 		
 			case 'C': 
 				printf("Digite um numero decimal:\n");
@@ -22,12 +27,27 @@ int main(){
 				dec2oct(a);
 				dec2hex(a);
 				break;
+				
 			case 'S':
-				printf("S\n");
+				printf("Digite a quantidade de variaveis:\n");
+				scanf("%d", &num_variaveis);
+				Matriz = alocaMatriz(num_variaveis, num_variaveis+1);
+				X = malloc(sizeof(double)*num_variaveis);
+				if(Matriz == NULL || X == NULL){ // Falta de memória
+					printf("Deu Pau!!\nFalta de memoria\n");
+					return 1;
+				}
+				leMatriz(Matriz, num_variaveis, num_variaveis+1);
+				imprimeMatriz(Matriz, num_variaveis, num_variaveis+1);
+				Jordan(Matriz, num_variaveis);
+				printf("\n\n");
+				imprimeMatriz(Matriz, num_variaveis, num_variaveis+1);
 				break;
+				
 			case 'E':
 				printf("E\n");
 				break;
+				
 			case 'F':
 				printf("Programa Encerrado\n");
 				return 0;
@@ -42,6 +62,7 @@ double* dec2bin(double n){
 	int  i, c=0;
 	int vetor_int[20], vetor_fra[20];
 	
+	/*Preenche os vetores da parte inteira e fracionaria da conversão dos bits com 0*/
 	for(i=0; i<20; i++){
 		vetor_fra[i] = 0;
 	}
@@ -53,7 +74,7 @@ double* dec2bin(double n){
 	// Parte fracionária do número
 	parte_fra = n - (int)n;
 	
-	//Transformando a parte fracionária do número decimal para binário
+	/*Transformando a parte fracionária do número decimal para binário*/
 	int j=0;
 	while(parte_fra!=0){
 		parte_fra *= 2;
@@ -252,4 +273,94 @@ double* dec2hex(double n){
 	
 	printf("\n");
 
+}
+
+
+
+double **alocaMatriz(int L, int C){
+/* Se houver memoria disponivevl a função aloca uma matriz
+bidimensional de double com L linhas e C colunas e devolve
+um ponteiro para essa matriz; Caso contrário, devolve um
+ponteiro nulo.*/
+
+	double **M;
+	int i, j;
+	M = malloc(sizeof(M) * L);
+	if (M == NULL){ // Falta de memória
+		return NULL;
+	}
+	
+	for(i = 0; i < L; i++){
+		M[i] = malloc(sizeof(double) * C);
+		
+		if(M[i] == NULL){
+			for(j = 0; j < i; j++){
+				free(M[j]);
+			}
+			free(M);
+			return NULL;
+		}
+		
+	}
+	return M;
+} // Fim **AlocaMatriz
+
+void leMatriz(double **M, int L, int C){
+/* Lê valores para uma matriz de double, alocada dinâmicamente
+com L linhas e C colunas. */
+
+	int i, j;
+	for(i = 0; i < L; i++){
+		for(j = 0; j < C; j++){
+			printf("M[%d][%d]: ", i+1, j+1);
+			scanf("%lf", &M[i][j]);
+		}
+	}
+} // Fim LêMatriz
+
+void imprimeMatriz(double **M, int L, int C){
+/* Imprime uma matriz de double com L linhas e C colunas*/
+	
+	int i, j;
+	for(i = 0; i < L; i++){
+		for(j = 0; j < C; j++){
+			printf("%10.4lf ", M[i][j]);
+		}
+		printf("\n");
+	}
+} // Fim imprimeMatriz
+
+
+
+void Jordan(double **M, int N){
+	int i, j, k, c;
+	double mult_baixo, mult_cima, aux;
+	int *x, aux2;
+	x = (int *) malloc(sizeof(int) * N);
+	
+	for(i = 0; i < N; i++) {
+		x[i] = i + 1;
+	}
+	
+	for(i = 0; i< N-1; i++){
+		if(M[i][i] == 0){ //Pivô sendo nulo
+			c = i + 1;
+			while(c < N && M[i][c] == 0){
+				c++;
+			}
+					
+			if(c < N){ // troca das colunas
+				aux2 = x[i];
+				x[i] = x[c];
+				x[c] = aux2;
+				for(j = 0 ; j < N; j++) {
+					aux = M[j][i];
+					M[j][i] = M[j][c];
+					M[j][c] = aux;
+				}
+			}
+		}
+		
+		
+	}
 }
